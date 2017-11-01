6f31225e3572728bc75e5e293a48f79f8cf76996
nwdehmlow <nwdehmlow@gmail.com>
2017-11-01 17:39:11 -0500

    final

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'Xoz2SfeY1WbcOY7EbCfIc2lS7',
+  consumer_secret: 'U5RvM5deFDD5cuv9LJ5ucFtjIsDT1z8w2TlAi0qW9O5Q6X5ROj',
+  access_token_key: '727956912520712192-27YTESkdgPOVGra7OlrKnjckm1tsKJ8',
+  access_token_secret: 'iX7g14Xlb8yKfNqxL5FI6kUCh7nin8177RYlGgJi6mZcg',
+}
+
+module.exports = twitterKeys;
